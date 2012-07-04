8aa0f3a767e4f33d38b05412d9c00c26da93861b
rbsl <rbsl@rbsl-Vostro-3550.(none)>
2012-07-04 11:59:18 +0530

    change in the content

--- payplans21.php
+++ payplans21.php
@@ -1,5 +1,9 @@
-<?php 
-echo "Hello payplans 2.1";
-echo "Hello payplans 2.1";
-?>
+<h2>PayPlans 2.0</h2>
 
+The latest released version of PayPlans is <input type=button onClick="parent.location='http://www.jpayplans.com'" value='2.1'>
+<h3>Main Highlights</h3>
+<ul>
+<li>PayPlans Installer to migrate from older version to PayPlans 2.0</li>
+<li>Introduced Wallet system Apps restructuring</li>
+<li>Performance improvement upto a great extent PDF invoice plugin is provided.</li>
+<li>Integration with Rok feature table</li></ul>
